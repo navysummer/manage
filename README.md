343f54bce751dccb97e6795b9eb5e12d00477409
navysummer <navysummer@yeah.net>
2019-11-28 17:25:00 +0800

    init

--- README.md
+++ README.md
@@ -10,18 +10,17 @@ print(code)
 2.通过二维码获取
 ```python
 from manage import GoogleAuthenticator
-sk = 'xxxxxxxxxxx'
-ga = GoogleAuthenticator(sk)
-QR_path = ''
+ga = GoogleAuthenticator()
+QR_path = 'chart.png'
 code = ga.QR2code(QR_path)
 print(code)
 ```
 # 获取授权列表
 ```python
 from manage import System
-ip = '127.0.0.1'
-phone = '137****3738'
-password = 'xxxxxx'
+ip = 'xxx.xxx.xxx.xxx'
+phone = '184xxxx0317'
+password = 'xxxxxxxxx'
 sy = System(ip,phone,password,sk)
 data = sy.getAuthList()
 print(data)
--- test.py
+++ test.py
@@ -1,13 +1,20 @@
 from manage import GoogleAuthenticator
-sk = 'cie5rsgznzqaxtkclxjrra4cxhkajuam'
+sk = 'xxxxxxxxxxxxxxxxxxx'
 ga = GoogleAuthenticator(sk)
 code = ga.getTotp()
 print(code)
 
+from manage import GoogleAuthenticator
+ga = GoogleAuthenticator()
+QR_path = 'chart.png'
+code = ga.QR2code(QR_path)
+print(code)
+
 from manage import System
-ip = '10.129.133.205'
-phone = '18401170317'
-password = 'ctsso'
+ip = 'xxx.xxx.xxx.xxx'
+phone = '184xxxx0317'
+password = 'xxxxxxxxx'
 sy = System(ip,phone,password,sk)
 data = sy.getAuthList()
 print(data)
+
