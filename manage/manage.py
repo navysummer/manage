dfd4f1cbf35d698677702ab59036c8353504f03f
navysummer <navysummer@yeah.net>
2019-12-03 10:56:52 +0800

    fix bug

--- manage/manage.py
+++ manage/manage.py
@@ -1,20 +1,29 @@
 # -*- coding: UTF-8 -*-
 import requests
 from .auth import GoogleAuthenticator
+try:
+    from urllib.parse import urlparse,parse_qs
+except ImportError:
+    from urlparse import urlparse,parse_qs
 
 class System(object):
     """docstring for System"""
-    def __init__(self,ip,phone,password,sk,port=80):
+    def __init__(self,url,phone,password,sk):
         super(System, self).__init__()
-        self.ip = ip
-        if port == 80:
-            self.__url = 'http://' + ip
-        else:
-            self.__url = 'http://'+ ip + ':' + port
+        self.urlinfo = urlparse(url)
+        Host = self.urlinfo.netloc
+        scheme = self.urlinfo.scheme
+        path = self.urlinfo.path
+        params = self.urlinfo.params
+        querys = self.urlinfo.query
+        queryDict = parse_qs(querys)
+        self.__url = scheme + "://" + Host
         self.phone = phone
         self.password = password
         self.__sk = sk
         self.session = None
+        self.cookies = None
+        self.__login()
 
     def __login(self):
         __login_url = self.__url + '/login'
@@ -26,7 +35,6 @@ class System(object):
             'Connection':'keep-alive',
             'Content-Length':'44',
             'Content-Type':'application/x-www-form-urlencoded',
-            'Host':self.ip,
             'Origin':self.__url,
             'Referer':__login_url,
             'Upgrade-Insecure-Requests':'1',
@@ -43,59 +51,34 @@ class System(object):
         if response.status_code == 302:
             cookies = response.cookies.get_dict()
             if not cookies:
-                raise Exception('get cookies fail')
+                raise Exception('login fail')
             if 'session' in cookies:
-                session = cookies['session']
-                return session
+                self.session = cookies['session']
+                self.cookies = cookies
             else:
-                raise Exception('get session fail')
+                raise Exception('login fail')
         else:
             self.__login()
         
 
     def getAuthList(self,page=1,limit=100):
         __authList_url = self.__url + '/system/auth/getSystemsByPage'
-        if self.session is None:
-            self.session = self.__login()
-        __headers = {
-            'Accept':'application/json, text/javascript, */*; q=0.01',
-            'Accept-Encoding':'gzip, deflate',
-            'Accept-Language':'zh-CN,zh;q=0.9,en;q=0.8',
-            'Connection':'keep-alive',
-            'Content-Length':'16',
-            'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
-            'Cookie':'session=' + self.session,
-            'Host':self.ip,
-            'Origin':self.__url,
-            'Referer':self.__url + '/system/authList',
-            'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
-            'X-Requested-With':'XMLHttpRequest'
-        }
+        if self.cookies is None:
+            raise Exception('login fail')
         __form_data = {
             'page':page,
             'limit':limit
         }
-        response = requests.post(__authList_url,data=__form_data,headers=__headers)
+        response = requests.post(__authList_url,data=__form_data,cookies=self.cookies)
         data = response.json()
         self.__logout()
         return data
 
     def __logout(self):
         __logout_url = self.__url + '/logout'
-        if self.session is None:
-            raise Exception('session not exist,logout fail')
-        __logout_headers = {
-            'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
-            'Accept-Encoding':'gzip, deflate',
-            'Accept-Language':'zh-CN,zh;q=0.9,en;q=0.8',
-            'Connection':'keep-alive',
-            'Cookie':'session=' + self.session,
-            'Host':self.ip,
-            'Referer':self.__url,
-            'Upgrade-Insecure-Requests':'1',
-            'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
-        }
-        response = requests.get(__logout_url,headers=__logout_headers)
+        if self.cookies is None:
+            raise Exception('login fail')
+        response = requests.get(__logout_url,cookies=self.cookies)
         if response.status_code == 200:
             return True
         else:
